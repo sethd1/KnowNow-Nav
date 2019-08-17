f98a775c753f47c245b95747236b9a8e66e24dbc
mjlomeli <mjlomeli@uci.edu>
2019-08-16 21:21:14 -0700

    Updated slice methods

--- Spreadsheet.py
+++ Spreadsheet.py
@@ -3,10 +3,11 @@
 """Spreadsheet
 
 Opens a CSV spreadsheet for reading and searching operations.
+
+Ideas: pickle handling with __call__ && __init__, print formatting,
 """
 from prettytable import PrettyTable
 from pathlib import Path
-import string
 import csv
 
 # default required values
@@ -80,11 +81,15 @@ class Spreadsheet:
         return [item[self.book[fieldname]] for item in self.spreadsheet]
 
     def find(self, value):
-        results = []
-        for row in self.spreadsheet:
-            if value in row.values():
-                results.append(row)
-        return results
+        return [row for row in self.spreadsheet if value in row]
+
+    def convertToDict(self, item):
+        if isinstance(item, list) and len(item) > 0:
+            if isinstance(item[0], list) and len(item[0]) > 0:
+                return [dict(zip(self.headers, value)) for value in item]
+            elif not isinstance(item[0], list):
+                return dict(zip(self.headers, item))
+        return None
 
     def textLength(self, text, length=DEFAULT_TEXT_LENGTH):
         if isinstance(text, list):
@@ -101,6 +106,15 @@ class Spreadsheet:
         self.headers = list(headers.values())
         self.book = {header: index for index, header in enumerate(self.headers)}
 
+    def __contains__(self, item):
+        if item in self.real_headers:
+            return True
+        else:
+            for row in self.spreadsheet:
+                if item in row:
+                    return True
+        return False
+
     def __getitem__(self, item):
         if isinstance(item, str):
             if self.headers is not None and item in self.headers:
@@ -115,7 +129,7 @@ class Spreadsheet:
                 return [element.values() for element in self.spreadsheet if element[pos1] == pos2]
             elif isinstance(pos1, int) and isinstance(pos2, str):
                 return self.spreadsheet[pos1 - 1][pos2]
-        elif isinstance(item, int):
+        elif isinstance(item, int) or isinstance(item, slice):
             return self.spreadsheet[item]
 
     def __len__(self):
@@ -132,6 +146,18 @@ class Spreadsheet:
         self.__index += 1
         return item
 
+    def __call__(self, spreadsheet=DEFAULT_SPREADSHEET):
+        with open(Path(spreadsheet), 'r', newline="", encoding="utf-8") as f:
+            content = csv.DictReader(f)
+            for element in content:
+                temp = list(element.values())
+                if temp not in self.spreadsheet:
+                    self.spreadsheet.append(temp)
+
+    def __format__(self, format_spec):
+        #TODO: "Sheet has at columns topic: {a}".format('column')"
+        pass
+
     def __str__(self):
         if self.table is not None:
             return str(self.table)
@@ -140,3 +166,15 @@ class Spreadsheet:
             for content in self.spreadsheet:
                 self.table.add_row(self.textLength(content))
             return str(self.table)
+
+
+if __name__ == '__main__':
+    print("Initializing with arguments: Spreadsheet(DEFAULT_SPREADSHEET, NORM_HEADERS)")
+    sheet = Spreadsheet(DEFAULT_SPREADSHEET, NORM_HEADERS)
+    sheet = Spreadsheet(DEFAULT_SPREADSHEET, NORM_HEADERS)
+    print("Initializing default constructor: Spreadsheet()")
+    sheet = Spreadsheet()
+
+    print("Iterating through Spreadsheet")
+    for row in sheet[:3]:
+        print(row)
