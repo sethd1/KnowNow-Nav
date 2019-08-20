83d0be585bde50d4ae129c73037d9c3fc4915d73
Mauricio <mjlomeli@uci.edu>
2019-08-20 07:49:27 -0700

    Chose the color for the cohort message in the posting.

---
+++ Neo4jDriver.py
@@ -0,0 +1,39 @@
+# !/usr/bin/env python
+
+"""Neo4jdriver
+
+If the description is long, the first line should be a short summary of Neo4jDriver.py
+that makes sense on its own, separated from the rest by a newline.
+"""
+
+from pathlib import Path
+from neo4j import GraphDatabase, basic_auth
+
+PATH = Path.cwd()
+
+__author__ = "Mauricio Lomeli"
+__date__ = "8/17/2019"
+__license__ = "MIT"
+__version__ = "0.0.0.1"
+__maintainer__ = "Mauricio Lomeli"
+__email__ = "mjlomeli@uci.edu"
+__status__ = "Prototype"
+
+
+driver = GraphDatabase.driver(
+    "bolt://34.203.33.130:38790",
+    auth=basic_auth("neo4j", "excuses-bush-reels"))
+session = driver.session()
+
+cypher_query = '''
+MATCH (n)
+RETURN id(n) AS id
+LIMIT 10
+'''
+
+results = session.run(cypher_query,
+  parameters={})
+
+for record in results:
+  print(record['id'])
+
